73f1bb7aadaa3e003f21d56dd4a84ec4cc28dcad
Lucas Oliveira <lucasmateusoliveira0@gmail.com>
2021-10-08 23:12:49 -0300

    Update shortenURL.ts

--- src/assets/ts/api/shortenURL.ts
+++ src/assets/ts/api/shortenURL.ts
@@ -12,7 +12,7 @@ export default async (url: string): Promise<string> => {
   if (response.ok) {
     const json: { short: string } = await response.json();
 
-    return `https://zws.im/${json.short}`;
+    return `https://w.fpt.icu/${json.short}`;
   } else {
     const json = await response.json();
 
